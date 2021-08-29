0ccbb3a4985c3f4d35835581b05e88e6a535bcc9
Sandesh <sandesh@Kumars-MacBook-Pro.local>
2021-08-29 11:34:42 +0545

    second commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+
+
+//main fn()
+export default function index () {
+//your variable, function and hooks go here
+
+//main return
+  return (
+    <>
+      <div className="text-green-400 text-3xl m-5 p-6 bg-pink-300 shadow-2xl  rounded-b-2xl">Hello from index</div>
+    </>
+  )
+};
